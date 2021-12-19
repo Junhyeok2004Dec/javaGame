3dc46cd0958256f235b6dd4aedf9b9967da6c5df
Junhyeok2004Dec <47268745+Junhyeok2004Dec@users.noreply.github.com>
2021-12-19 23:43:27 +0900

    maybe finished in 12/19

--- src/main/MainScene.java
+++ src/main/MainScene.java
@@ -1,68 +1,11 @@
 package main;
 
-import main.assets.Shader;
-import main.assets.Texture;
 import main.assets.components.SpriteRenderer;
 import main.util.AssetPool;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
 
 public class MainScene extends Scene {
-
-
-    private String vertexShaderSource = "#version 330 core\n" +
-            "layout (location = 0) in vec3 aPos;\n" +
-            "layout (location = 1) in vec4 aColor;\n" +
-            "\n" +
-            "out vec4 fColor;\n" +
-            "\n" +
-            "void main()\n" +
-            "{\n" +
-            "    fColor = aColor;\n" +
-            "    gl_Position = vec4(aPos, 1.0);\n" +
-            "}\n";
-    private String fragmentShaderSource = "#version 330 core\n" +
-            "\n" +
-            "in vec4 fColor;\n" +
-            "\n" +
-            "out vec4 color;\n" +
-            "\n" +
-            "void main()\n" +
-            "{\n" +
-            "    color = fColor;\n" +
-            "}\n";
-
-    private int vertexID, fragmentID, shaderProgram;
-
-    private float[] vertexArray = {
-            // position               // color                  // UV Coordinates
-            100f, 0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1, 1, // Bottom right 0
-            0f, 100f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0, 0, // Top left     1
-            100f, 100f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1, 0, // Top right    2
-            0f, 0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0, 1  // Bottom left  3
-    };
-
-    // IMPORTANT: Must be in counter-clockwise order
-    private int[] elementArray = {
-            /*
-                    x        x
-                    x        x
-             */
-            2, 1, 0, // Top right triangle
-            0, 1, 3 // bottom left triangle
-    };
-
-
-    private int vaoID, vboID, eboID;
-
-
-    private Shader defaultShader;
-    private Texture texture;
-
-    GameObject testObj;
-
-    private boolean firstTime = false;
-
     public MainScene() {
 
 
